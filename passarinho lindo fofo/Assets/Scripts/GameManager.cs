ceec338db449bce7550519708a102ad5fdceb147
fermariah <mariah.fe.santos.soares@gmail.com>
2025-09-16 08:50:19 -0300

    UI e GameManager
    os scripts já estão configurados, só falta colocá-los na cena

---
+++ passarinho lindo fofo/Assets/Scripts/GameManager.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    #region Singleton
+    public static GameManager instance;
+
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+        else if (instance != this)
+        {
+            Destroy(gameObject);
+        }
+
+        screenBounds = Camera.main.ScreenToWorldPoint(new Vector2(Screen.width, Screen.height));
+    }
+    #endregion
+
+    Vector2 screenBounds;
+    int score;
+
+    public Vector2 ScreenBounds { get => screenBounds; }
+    public int Score { get => score; set => score = value; }
+}
